79bf0654cece90634f988ef3507afba60d920443
Raven <152022920+Raven-002@users.noreply.github.com>
2025-08-16 19:21:48 +0300

    Update DEFAULT_AXIS_STEPS_PER_UNIT for improved extruder calibration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1300,7 +1300,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 1000 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 920 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
